2dde949e2d8cbb619979356e8409de185cba6071
FaridSa <FaridSa@DESKTOP-AGVTG9C>
2024-09-20 21:10:46 +0330

    added un implemented file repo

---
+++ Tax/CongestionTax/Src/Calculator.Infrastructure/File/FileRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator.Infrastructure.File
+{
+    using Calculator.Domain.Entity;
+    using Calculator.Domain.Repository;
+    public class FileRepository : IRepository
+    {
+        public Task<IDictionary<int, IRuleSheet>> GetRuleSheetAsync(ICity city, IEnumerable<int> Years, CancellationToken cancellationToken)
+           => throw new NotImplementedException();
+    }
+}
