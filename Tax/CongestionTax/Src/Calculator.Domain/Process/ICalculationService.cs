e526f0945cf074d927218fc2e4486aaa57849671
FaridSa <FaridSa@DESKTOP-AGVTG9C>
2024-09-22 01:37:52 +0330

    manage packages and its all DONE

--- Tax/CongestionTax/Src/Calculator.Domain/Process/ICalculationService.cs
+++ Tax/CongestionTax/Src/Calculator.Domain/Process/ICalculationService.cs
@@ -1,7 +1,6 @@
 ﻿namespace Calculator.Domain.Process
 {
     using Calculator.Domain.Entity;
-    using ErrorOr;
     public interface ICalculationService
     {
         Task<IEnumerable<DateTax>> CalculateAsync(ICity city, IVehicle vehicle, IEnumerable<DateTime> dateTimes, CancellationToken cancellationToken);
