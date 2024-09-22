26a04ea5801496189d0670f35412c49f480dc8d5
FaridSa <FaridSa@DESKTOP-AGVTG9C>
2024-09-23 00:23:32 +0330

    some rename , last commit.

--- Tax/CongestionTax/Src/Calculator.Infrastructure/Database/DataBaseRepository.cs
+++ Tax/CongestionTax/Src/Calculator.Infrastructure/Database/DataBaseRepository.cs
@@ -15,7 +15,7 @@
 
         public async Task<IDictionary<int, IRuleSheet>> GetRuleSheetAsync(ICity city, IEnumerable<int> years, CancellationToken cancellationToken)
         {
-            //perhaps we can reduce query request maybe later
+            //perhaps we can reduce query requests , maybe later
 
             var cityYearBaseQuerable = appDbContext.City
                 .AsNoTracking()
@@ -24,41 +24,44 @@
                 .Where(x => years.Contains(x.cyc.Year))
                 .AsSplitQuery();
 
-            var tollRateIntervalCombinedData = await cityYearBaseQuerable
+            var taxRateIntervalCombinedData = await cityYearBaseQuerable
                 .Join(appDbContext.CycTaxRateInterval, s => s.cyc.Id, tri => tri.CityYearCurrencyId, (s, tri) => new { cycId = s.cyc.Id, tri })
                 .ToListAsync(cancellationToken)
                 .ConfigureAwait(false);
-
-            var tollRateByYear = tollRateIntervalCombinedData
-                .GroupBy(x => x.cycId)
-                .ToDictionary(k => k.Key, v => v.Select(i => i.tri).OrderBy(o => o.From).AsEnumerable());
 
             var taxFreeVehicleCombinedData = await cityYearBaseQuerable
                 .Join(appDbContext.CycTaxFreeVehicleType, s => s.cyc.Id, tfvt => tfvt.CityYearCurrencyId, (s, tfvt) => new { cycId = s.cyc.Id, tfvt.VehicleType!.Type })
                 .ToListAsync(cancellationToken)
                 .ConfigureAwait(false);
 
-            var taxFreeVehicleByYear = taxFreeVehicleCombinedData?.GroupBy(x => x.cycId).ToDictionary(k => k.Key, v => v.Select(i => i.Type));
-
             var taxFreeDatePeriodCombinedData = await cityYearBaseQuerable
-               .Join(appDbContext.CycTaxFreeDatePeriod, s => s.cyc.Id, tfdp => tfdp.CityYearCurrencyId, (s, tfdp) => new { cycId = s.cyc.Id, tfdp })
-               .ToListAsync(cancellationToken)
-               .ConfigureAwait(false);
+                .Join(appDbContext.CycTaxFreeDatePeriod, s => s.cyc.Id, tfdp => tfdp.CityYearCurrencyId, (s, tfdp) => new { cycId = s.cyc.Id, tfdp })
+                .ToListAsync(cancellationToken)
+                .ConfigureAwait(false);
 
-            var taxFreeDatePeriodByYear = taxFreeDatePeriodCombinedData?.GroupBy(x => x.cycId).ToDictionary(k => k.Key, v => v.Select(i => i.tfdp));
+            var taxRateByYear = taxRateIntervalCombinedData
+                .GroupBy(x => x.cycId)
+                .ToDictionary(k => k.Key, v => v.Select(i => i.tri)
+                .OrderBy(o => o.From).AsEnumerable());
+
+            var taxFreeVehicleByYear = taxFreeVehicleCombinedData?
+                .GroupBy(x => x.cycId)
+                .ToDictionary(k => k.Key, v => v.Select(i => i.Type));
+
+            var taxFreeDatePeriodByYear = taxFreeDatePeriodCombinedData?
+                .GroupBy(x => x.cycId)
+                .ToDictionary(k => k.Key, v => v.Select(i => i.tfdp));
 
             return await cityYearBaseQuerable
             .Join(appDbContext.CycRuleSheet, s => s.cyc.Id, cycr => cycr.CityYearCurrencyId, (s, cycr) => new { s.c, s.cyc, cycr })
-
             .GroupJoin(appDbContext.HolidayTaxFreePeriod, s => s.cycr.Id, htfp => htfp.CityYearCurrencyRuleSheetId, (s, htfp) => new { s.c, s.cyc, s.cycr, htfp })
             .SelectMany(g => g.htfp.DefaultIfEmpty(), (s, htfp) => new { s.c, s.cyc, s.cycr, htfp })
-
             .Select(x => new RuleSheet()
             {
                 City = x.c,
                 Year = x.cyc.Year,
                 CurrencyUnit = x.cyc.CurrencyUnit,
-                TaxRateIntervals = tollRateByYear[x.cyc.Id],
+                TaxRateIntervals = taxRateByYear[x.cyc.Id],
                 PublicHolidays = holidayRepository.GetPublicHolidays(x.c, x.cyc.Year),
                 TaxFreePeriods = taxFreeDatePeriodByYear != null ? taxFreeDatePeriodByYear[x.cyc.Id] : null,
                 TaxFreeVehicleTypes = taxFreeVehicleByYear != null ? taxFreeVehicleByYear[x.cyc.Id] : null,
